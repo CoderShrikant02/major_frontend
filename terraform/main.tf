b750ca84d1252cddbc08fa4bf54438912e69fb00
Shrikant Chiddarwar <shrikantchiddarwar02@gmail.com>
2026-03-13 23:43:26 +0530

    Add auto deploy via user_data

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,12 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# -----------------------------
+# Security Group
+# -----------------------------
 resource "aws_security_group" "secure_sg" {
   name        = "secure-security-group"
-  description = "Secure security group for EC2"
+  description = "Secure access for EC2 instance"
 
   ingress {
     description = "SSH access from my IP only"
@@ -12,16 +15,24 @@ resource "aws_security_group" "secure_sg" {
     to_port     = 22
     protocol    = "tcp"
 
-    cidr_blocks = ["182.76.246.162/32"]   # your public IP
+    cidr_blocks = ["182.76.246.162/32"]
+  }
+
+  ingress {
+    description = "Flask App Access"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    description = "Allow HTTPS outbound only"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
 
-    cidr_blocks = ["182.76.246.162/32"]   # restricted outbound
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -29,7 +40,11 @@ resource "aws_security_group" "secure_sg" {
   }
 }
 
+# -----------------------------
+# EC2 Instance
+# -----------------------------
 resource "aws_instance" "tomato_server" {
+
   ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t3.micro"
 
@@ -42,6 +57,25 @@ resource "aws_instance" "tomato_server" {
   root_block_device {
     encrypted = true
   }
+
+  # Auto Deploy App
+  user_data = <<-EOF
+              #!/bin/bash
+
+              apt update -y
+              apt install -y python3-pip git
+
+              cd /home/ubuntu
+
+              git clone https://github.com/CoderShrikant02/major_frontend.git
+
+              cd major_frontend
+
+              pip3 install -r requirements.txt
+
+              nohup python3 app.py --host 0.0.0.0 --port 5000 > app.log 2>&1 &
+
+              EOF
 
   tags = {
     Name = "tomato-ai-server"
