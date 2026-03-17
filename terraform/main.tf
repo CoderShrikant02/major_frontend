371433b268b033c347e389780292977b0e8fc753
Shrikant Chiddarwar <shrikantchiddarwar02@gmail.com>
2026-03-17 20:12:28 +0530

    Deploy full project

--- terraform/main.tf
+++ terraform/main.tf
@@ -152,18 +152,10 @@ resource "aws_security_group" "secure_sg" {
   }
 
   egress {
-    description = "Allow outbound HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = "Allow outbound HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    description = "TEMP: allow outbound to the internet for provisioning"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -252,6 +244,7 @@ if ! swapon --show | grep -q .; then
   echo '/swapfile none swap sw 0 0' >> /etc/fstab
 fi
 
+run bash -c 'echo "Acquire::ForceIPv4 \\"true\\";" > /etc/apt/apt.conf.d/99force-ipv4'
 run retry 5 apt-get update -y
 # Use Ubuntu repo packages for reliability.
 run retry 5 apt-get install -y --no-install-recommends ca-certificates curl git git-lfs docker.io docker-compose awscli
